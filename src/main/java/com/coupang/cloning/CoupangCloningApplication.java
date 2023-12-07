ee330f5c52abf4554cc2c23aa1f3fcc45a9cda8c
yong <yoho9908@gmail.com>
2023-12-07 18:06:49 +0900

    init

---
+++ src/main/java/com/coupang/cloning/CoupangCloningApplication.java
@@ -0,0 +1,13 @@
+package com.coupang.cloning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoupangCloningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoupangCloningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coupang/cloning/CoupangCloningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coupang.cloning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoupangCloningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
